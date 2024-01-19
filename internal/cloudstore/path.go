a53c69365ec72f7138b3e528633e50e1d4c62120
edward ciccone <ciccone.etp@gmail.com>
2024-01-19 13:04:41 -0600

    Implemented PathMapper.
    FindDir accepts a directory path and returns its ID. Directory path must belong to a specific user and reside under their root directory.

---
+++ internal/cloudstore/path.go
@@ -0,0 +1,70 @@
+package cloudstore
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"net/http"
+	"path/filepath"
+	"strings"
+
+	"github.com/cicconee/clox/internal/app"
+)
+
+// PathMapper maps user-facing paths to the path structure on the server.
+type PathMapper struct{}
+
+// NewPathMapper creates a new PathMapper.
+func NewPathMapper() *PathMapper {
+	return &PathMapper{}
+}
+
+// DirSearch are the parameters for finding a directory ID based on the
+// Path, that belongs to a specific user (UserID), under their root
+// directory (RootID).
+type DirSearch struct {
+	UserID string
+	RootID string
+	Path   string
+}
+
+// FindDir parses a path to a directory and searches for the ID. The directory
+// names in the path are mapped to the ID's on the server. If found, the ID
+// is returned.
+//
+// The directory must belong to the user (userID) and live under their
+// root directory (rootID).
+func (pm *PathMapper) FindDir(ctx context.Context, q *Query, d DirSearch) (string, error) {
+	fp := filepath.Clean(d.Path)
+	var p string
+	if fp == "." || fp == "/" {
+		p = "root"
+	} else if fp[0] == '/' {
+		p = "root" + fp
+	} else {
+		p = "root/" + fp
+	}
+	paths := strings.Split(p, "/")
+
+	directoryID := d.RootID
+	for i := 1; i < len(paths); i++ {
+		pName := paths[i]
+		dir, err := q.SelectDirectoryByUserNameParent(ctx, d.UserID, pName, directoryID)
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				return "", app.Wrap(app.WrapParams{
+					Err:         fmt.Errorf("directory %q does not exist [path: %s]", pName, d.Path),
+					SafeMessage: fmt.Sprintf("Directory %q does not exist", strings.Join(paths[1:i+1], "/")),
+					StatusCode:  http.StatusBadRequest,
+				})
+			}
+
+			return "", err
+		}
+
+		directoryID = dir.ID
+	}
+
+	return directoryID, nil
+}
